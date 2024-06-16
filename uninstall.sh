4332fcf30656febfd0101bafd568871039ea0286
Agustin Nieto <agusnieto.gg@gmail.com>
2024-06-16 01:34:20 +0000

    Update install scripts

--- uninstall.sh
+++ uninstall.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env bash
 
-sudo systemctl stop powerwatcher.timer
-sudo systemctl disable powerwatcher.timer
+sudo systemctl stop powerwatcher.service
+sudo systemctl disable powerwatcher.service
 files=$(find src -type f | sed 's/^src//g')
 sudo rm $files
